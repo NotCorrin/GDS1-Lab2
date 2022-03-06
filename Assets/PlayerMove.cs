d209929a85bdd420ec4303f83043858bd770909c
BillingtonBigCat <wmajor21@gmail.com>
2022-03-07 01:24:25 +1100

    FireFlower power working
    need to test with working rigidbody character

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -13,32 +13,41 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     void Update()
     {
-        curmaxspeed = (Input.GetKey(KeyCode.LeftShift)?speed:runspeed);
+        curmaxspeed = (Input.GetKey(KeyCode.LeftShift) ? speed : runspeed);
         move = Input.GetAxisRaw("Horizontal");
         curspeed.x += move * acc * curmaxspeed;
-        if(Mathf.Abs(curspeed.x) > curmaxspeed || (move == 0 && Mathf.Abs(curspeed.x) > Time.deltaTime * acc)) 
+        if (Mathf.Abs(curspeed.x) > curmaxspeed || (move == 0 && Mathf.Abs(curspeed.x) > Time.deltaTime * acc))
         {
             curspeed.x -= Mathf.Sign(curspeed.x) * acc * 0.5f;
-            if(Mathf.Abs(curspeed.x) < acc) curspeed.x = 0;
+            if (Mathf.Abs(curspeed.x) < acc) curspeed.x = 0;
         }
         curspeed.x = Mathf.Clamp(curspeed.x, -curmaxspeed, curmaxspeed);
-        if(grounded == false) curspeed.y -= gravity;
-        if(Input.GetKeyDown(KeyCode.W) && grounded == true)
+        if (grounded == false) curspeed.y -= gravity;
+        if (Input.GetKeyDown(KeyCode.W) && grounded == true)
         {
             curspeed.y = jumpspeed;
             grounded = false;
         }
         curspeed.y = Mathf.Clamp(curspeed.y, -maxfallspeed, jumpspeed);
         transform.position += (Vector3)curspeed * Time.deltaTime;
-        while(Physics2D.OverlapBox((Vector2)transform.position + col.offset, col.size * transform.localScale, 0))
+        while (Physics2D.OverlapBox((Vector2)transform.position + col.offset, col.size * transform.localScale, 0))
         {
             curspeed.y = 0;
             grounded = true;
-            transform.position += Vector3.up * 0.02f;           
+            transform.position += Vector3.up * 0.02f;
         }
     }
     void LateUpdate()
     {
         rend.flipX = curspeed.x < 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "FireFlower")
+        {
+            Destroy(other.gameObject);
+            GameManager.PickUpFireFlower();
+        }
     }
 }
