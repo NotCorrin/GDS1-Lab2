f432f8f66eaf8aa143e72793a16b359c98c9addc
H A <hugoa3520@gmail.com>
2022-03-07 00:02:57 +1100

    rewrote character controller

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -2,43 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMove : MonoBehaviour
 {
-    public float gravity = 1, jumpspeed = 1, maxfallspeed = 1.5f, acc = 1, speed = 5, runspeed = 8.5f, curmaxspeed;
-    public Vector2 curspeed;
+    public float jumpspeed = 1, acc = 1, maxfallspeed = 1, speed = 5, runspeed = 8.5f, curmaxspeed;
     public float move;
     public bool grounded;
+    public Rigidbody2D rb;
+    public bool capoff;
     public SpriteRenderer rend;
-    public BoxCollider2D col;
     // Start is called before the first frame update
     void Update()
     {
-        curmaxspeed = (Input.GetKey(KeyCode.LeftShift)?speed:runspeed);
+        curmaxspeed = (Input.GetKey(KeyCode.LeftShift)?runspeed:speed);
         move = Input.GetAxisRaw("Horizontal");
-        curspeed.x += move * acc * curmaxspeed;
-        if(Mathf.Abs(curspeed.x) > curmaxspeed || (move == 0 && Mathf.Abs(curspeed.x) > Time.deltaTime * acc)) 
+        rb.AddForce(move * acc * Vector3.right * Time.deltaTime, ForceMode2D.Impulse);
+        //if(Mathf.Sign(curspeed.x) > curmaxspeed || (move == 0 && Mathf.Sign(curspeed.x) > Time.deltaTime * acc)) curspeed.x -= Time.deltaTime * Mathf.Sign(curspeed.x) * acc;
+        rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -curmaxspeed, curmaxspeed), Mathf.Clamp(rb.velocity.y, -maxfallspeed, jumpspeed));
+        if(grounded == true)
         {
-            curspeed.x -= Mathf.Sign(curspeed.x) * acc * 0.5f;
-            if(Mathf.Abs(curspeed.x) < acc) curspeed.x = 0;
+            if(Mathf.Abs(rb.velocity.x) > curmaxspeed || (move == 0 && Mathf.Abs(rb.velocity.x) > Time.deltaTime * acc)) 
+            {
+                //rb.velocity -= Mathf.Sign(rb.velocity.x) * acc * 0.5f * Vector2.right;
+                rb.AddForce(Mathf.Sign(rb.velocity.x) * acc * 0.5f * Vector2.left);
+                if(Mathf.Abs(rb.velocity.x) < 0.1f) rb.velocity = Vector2.zero;
+            }
         }
-        curspeed.x = Mathf.Clamp(curspeed.x, -curmaxspeed, curmaxspeed);
-        if(grounded == false) curspeed.y -= gravity;
         if(Input.GetKeyDown(KeyCode.W) && grounded == true)
         {
-            curspeed.y = jumpspeed;
+            rb.AddForce((jumpspeed + Mathf.Abs(rb.velocity.x) * 3) * Vector2.up, ForceMode2D.Impulse);
             grounded = false;
         }
-        curspeed.y = Mathf.Clamp(curspeed.y, -maxfallspeed, jumpspeed);
-        transform.position += (Vector3)curspeed * Time.deltaTime;
-        while(Physics2D.OverlapBox((Vector2)transform.position + col.offset, col.size * transform.localScale, 0))
-        {
-            curspeed.y = 0;
-            grounded = true;
-            transform.position += Vector3.up * 0.02f;           
+        if((Input.GetKeyUp(KeyCode.W) || rb.velocity.y < 0) && grounded == false) capoff = true;
+        if(capoff) {
+            rb.AddForce(Vector2.down * 4);
+            if(rb.velocity.y > 0)rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.9f);
         }
     }
     void LateUpdate()
     {
-        rend.flipX = curspeed.x < 0;
+        rend.flipX = rb.velocity.x < 0;
+    }
+    void OnCollisionEnter2D()
+    {
+        grounded = true;
+        capoff = false;
     }
 }
